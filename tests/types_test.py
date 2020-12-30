5ce05daef90463e1c14c347ecb67c6cf66bd4aa9
Dermot Duffy <dermot.duffy@gmail.com>
2020-12-29 22:19:15 -0800

    Test corner cases for stats.

--- tests/types_test.py
+++ tests/types_test.py
@@ -40,5 +40,10 @@ def test_json_to_stats(caplog):
         {"total_size": 1709.0, "total_file_count": 1.0, "total_blob_count": None}
     ) == ResticStats(total_size=1709, total_file_count=1, total_blob_count=None)
 
+    assert json_to_stats({"foo": "bar"}) is None
+    assert "Skipping restic stats with missing key" in caplog.text
+
     assert json_to_stats({"total_size": "str", "total_file_count": 1, "total_blob_count": 4}) is None
     assert "Skipping restic stats with invalid value" in caplog.text
+
+    assert json_to_stats(None) is None
