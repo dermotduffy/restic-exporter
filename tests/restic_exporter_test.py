4de20efe4850bd60cd3cc2ee1a62bd636c1fa852
Dermot Duffy <dermot.duffy@gmail.com>
2020-12-29 19:44:36 -0800

    Add empty tests

---
+++ tests/restic_exporter_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""Test for the Hyperion Client."""
+
+import json
+import os
+import pytest
+import logging
+import string
+from typing import cast, Any, Dict, List, Optional, Tuple
+
+logging.basicConfig()
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.setLevel(logging.DEBUG)
+
+
+def test():
+    pass
